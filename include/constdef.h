4a06980dc00cd79b9c6758939213bd2f4c4e7813
Aleksandar Kuktin <akuktin@gmail.com>
2012-12-13 11:47:08 +0100

    Tweaked the macro constants.

--- include/constdef.h
+++ include/constdef.h
@@ -1,12 +1,24 @@
 #ifndef NBWORKS_CONSTDEF_H
 # define NBWORKS_CONSTDEF_H 1
 
-# ifndef NULL
-#  define NULL 0
+# ifdef NULL
+#  undef NULL
 # endif
+# define NULL 0
 
-# ifndef ONES
-#  define ONES (~0)
+# ifdef ONES
+#  undef ONES
 # endif
+# define ONES (~0)
+
+# ifdef TRUE
+#  undef TRUE
+# endif
+# define TRUE 1
+
+# ifdef FALSE
+#  undef FALSE
+# endif
+# define FALSE 0
 
 #endif /* NBWORKS_CONSTDEF_H */
