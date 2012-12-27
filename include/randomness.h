86429518fc77369dd6da41e9807a47d45faa4265
Aleksandar Kuktin <akuktin@gmail.com>
2012-12-27 14:19:43 +0100

    Added the first weak randomness routine.

---
+++ include/randomness.h
@@ -0,0 +1,6 @@
+#ifndef NBWORKS_RANDOMNESS_H
+# define NBWORKS_RANDOMNESS_H 1
+
+uint32_t make_weakrandom();
+
+#endif /* NBWORKS_RANDOMNESS_H */
---
+++ src/randomness.c
@@ -0,0 +1,19 @@
+#include "c_lang_extensions.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+struct {
+  uint64_t period;
+  unsigned int weakstate;
+} nbworks_random_state;
+
+uint32_t make_weakrandom() {
+  if (! nbworks_random_state.weakstate) {
+    nbworks_random_state.weakstate = time(0);
+  }
+
+  return rand_r(&(nbworks_random_state.weakstate));
+}
