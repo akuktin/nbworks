70182a713fe5119192036c32c307152749e401b3
Aleksandar Kuktin <akuktin@gmail.com>
2013-01-04 17:21:58 +0100

    Updated all functions in name_srvc_func.c, added name_srvc_B_callout_name().

--- src/name_srvc_func.c
+++ src/name_srvc_func.c
@@ -30,9 +30,10 @@ int name_srvc_B_add_name(unsigned char *name,
 			 uint32_t my_ip_address,
 			 int isgroup) {
   struct timespec sleeptime;
+  struct sockaddr_in addr;
   struct ss_queue *trans;
   struct name_srvc_packet *pckt, *outside_pckt;
-  struct sockaddr_in addr;
+  struct name_srvc_resource_lst *res;
   int result, i;
   uint16_t tid;
 
@@ -58,6 +59,7 @@ int name_srvc_B_add_name(unsigned char *name,
   trans = ss_register_name_tid(tid);
   if (! trans) {
     /* TODO: errno signaling stuff */
+    destroy_name_srvc_pckt(pckt, 0, 1);
     return -1;
   }
     
@@ -85,11 +87,25 @@ int name_srvc_B_add_name(unsigned char *name,
 					  OPCODE_REGISTRATION)) &&
 	(outside_pckt->header->nm_flags & FLG_AA) &&
 	(outside_pckt->header->rcode != 0)) {
-      /* This is a NEGATIVE NAME REGISTRATION RESPONSE. */
-      /* Failed. */
-      result = outside_pckt->header->rcode;
-      destroy_name_srvc_pckt(outside_pckt, 1, 1);
-      break;
+      res = outside_pckt->answers;
+      while (res) {
+	if ((0 == cmp_nbnodename(pckt->questions->qstn->name,
+				 res->res->name)) &&
+	    (pckt->questions->qstn->qtype ==
+	     res->res->rrtype) &&
+	    (pckt->questions->qstn->qclass ==
+	     res->res->rrclass)) {
+	  /* This is a relevant NEGATIVE NAME REGISTRATION RESPONSE. */
+	  /* Failed. */
+	  result = outside_pckt->header->rcode;
+	  break;
+	} else
+	  res = res->next;
+      }
+      if (res) {
+	destroy_name_srvc_pckt(outside_pckt, 1, 1);
+	break;
+      }
     }
 
     destroy_name_srvc_pckt(outside_pckt, 1, 1);
@@ -99,6 +115,168 @@ int name_srvc_B_add_name(unsigned char *name,
     /* Succeded. */
     pckt->header->opcode = OPCODE_REQUEST | OPCODE_REFRESH;
     ss_name_send_pckt(pckt, &addr, trans);
+  }
+
+  ss_deregister_name_tid(tid);
+  ss_name_dstry_recv_queue(trans);
+  free(trans);
+
+  destroy_name_srvc_pckt(pckt, 0, 1);
+
+  return result;
+}
+
+/* return: 0=success, >0=fail, -1=error */
+int name_srvc_B_release_name(unsigned char *name,
+			     unsigned char name_type,
+			     struct nbnodename_list *scope,
+			     uint32_t my_ip_address,
+			     int isgroup) {
+  struct timespec sleeptime;
+  struct ss_queue *trans;
+  struct name_srvc_packet *pckt;
+  struct sockaddr_in addr;
+  int i;
+  uint16_t tid;
+
+  /* TODO: change this to a global setting. */
+  sleeptime.tv_sec = 0;
+  sleeptime.tv_nsec = T_250MS;
+
+  addr.sin_family = AF_INET;
+  /* VAXism below. */
+  fill_16field(137, (unsigned char *)&(addr.sin_port));
+  addr.sin_addr.s_addr = INADDR_BROADCAST;
+
+  pckt = name_srvc_make_name_reg_big(name, name_type, scope, 0,
+				     my_ip_address, isgroup, 'B');
+  if (! pckt) {
+    /* TODO: errno signaling stuff */
+    return -1;
+  }
+
+  tid = make_weakrandom();
+
+  trans = ss_register_name_tid(tid);
+  if (! trans) {
+    /* TODO: errno signaling stuff */
+    destroy_name_srvc_pckt(pckt, 0, 1);
+    return -1;
+  }
+
+  /* Don't listen for incoming packets. */
+  ss_set_inputdrop_name_tid(tid);
+  ss_name_dstry_recv_queue(trans);
+
+  pckt->header->transaction_id = tid;
+  pckt->header->opcode = OPCODE_REQUEST | OPCODE_RELEASE;
+  pckt->header->nm_flags = FLG_B;
+
+  ss_name_send_pckt(pckt, &addr, trans);
+
+  for (i=0; i < (BCAST_REQ_RETRY_COUNT -1); i++) {
+    nanosleep(&sleeptime, 0);
+    ss_name_send_pckt(pckt, &addr, trans);
+  }
+
+  ss_deregister_name_tid(tid);
+  free(trans);
+
+  destroy_name_srvc_pckt(pckt, 0, 1);
+
+  return 0;
+}
+
+struct name_srvc_resource *name_srvc_B_callout_name(unsigned char *name,
+						    unsigned char name_type,
+						    struct nbnodename_list *scope) {
+  struct timespec sleeptime;
+  struct sockaddr_in addr;
+  struct name_srvc_resource_lst *res;
+  struct ss_queue *trans;
+  struct name_srvc_packet *pckt, *outside_pckt;
+  struct name_srvc_resource *result;
+  int i;
+  uint16_t tid;
+
+  result = 0;
+  /* TODO: change this to a global setting. */
+  sleeptime.tv_sec = 0;
+  sleeptime.tv_nsec = T_250MS;
+
+  addr.sin_family = AF_INET;
+  /* VAXism below. */
+  fill_16field(137, (unsigned char *)&(addr.sin_port));
+  addr.sin_addr.s_addr = INADDR_BROADCAST;
+
+  pckt = name_srvc_make_name_qry_req(name, name_type, scope);
+  if (! pckt) {
+    /* TODO: errno signaling stuff */
+    return 0;
+  }
+
+  tid = make_weakrandom();
+
+  trans = ss_register_name_tid(tid);
+  if (! trans) {
+    /* TODO: errno signaling stuff */
+    destroy_name_srvc_pckt(pckt, 0, 1);
+    return 0;
+  }
+
+  pckt->header->transaction_id = tid;
+  pckt->header->opcode = OPCODE_REQUEST | OPCODE_QUERY;
+  pckt->header->nm_flags = FLG_B;
+
+  for (i=0; i < BCAST_REQ_RETRY_COUNT; i++) {
+    ss_name_send_pckt(pckt, &addr, trans);
+
+    nanosleep(&sleeptime, 0);
+
+    ss_set_inputdrop_name_tid(tid);
+
+    while (1) {
+      outside_pckt = ss_name_recv_pckt(trans);
+      if (! outside_pckt) {
+	break;
+      }
+
+      if ((outside_pckt->header->opcode == (OPCODE_RESPONSE |
+					    OPCODE_QUERY)) &&
+	  (outside_pckt->header->nm_flags & FLG_AA) &&
+	  (outside_pckt->header->rcode != 0)) {
+	/* POSITIVE NAME QUERY RESPONSE */
+	res = outside_pckt->answers;
+
+	while (res) {
+	  if ((0 == cmp_nbnodename(pckt->questions->qstn->name,
+				   res->res->name)) &&
+	      (pckt->questions->qstn->qtype ==
+	       res->res->rrtype) &&
+	      (pckt->questions->qstn->qclass ==
+	       res->res->rrclass) &&
+	      (res->res->rdata_t == nb_address_list)) {
+	    /* This is what we are looking for. */
+
+	    result = res->res;
+	    res->res = 0;
+	    res = 0;
+	    break;
+
+	  }
+	}
+      }
+
+      destroy_name_srvc_pckt(outside_pckt, 1, 1);
+
+      if (result)
+	break;
+    }
+
+    if (result)
+      break;
+
+    ss_set_normalstate_name_tid(tid);
   }
 
   ss_deregister_name_tid(tid);
