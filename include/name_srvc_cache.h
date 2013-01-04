e3e96eb964def332b7f0a15f20e06e4e0ea8db0b
Aleksandar Kuktin <akuktin@gmail.com>
2013-01-04 22:46:18 +0100

    Someway through the cache implementation.

---
+++ include/name_srvc_cache.h
@@ -0,0 +1,59 @@
+#ifndef NBWORKS_NAMESRVCCACHE_H
+# define NBWORKS_NAMESRVCCACHE_H 1
+
+# include <time.h>
+
+# include "nodename.h"
+
+struct ipv4_addr_list {
+  uint32_t ip_addr;
+  struct ipv4_addr_list *next;
+};
+
+struct cache_scopenode {
+  struct nbnodename_list *scope;
+  struct cache_namenode *names;
+  struct cache_scopenode *next;
+};
+
+struct cache_namenode {
+  void *name;
+  unsigned char namelen;
+  unsigned char node_type;
+  int isgroup;
+  uint16_t dns_type;
+  uint16_t dns_class;
+  time_t timeof_death;
+  time_t endof_conflict_chance;
+  struct ipv4_addr_list *addrlist;
+  struct cache_namenode *next;
+};
+
+void init_name_srvc_cache();
+
+struct cache_scopenode *
+  add_scope(struct nbnodename_list *scope,
+            struct cache_namenode *first_node);
+struct cache_scopenode *
+  find_scope(struct nbnodename_list *scope);
+
+void *
+  prune_scopes(void *);
+
+struct cache_namenode *
+  add_name(struct cache_namenode *name,
+           struct nbnodename_list *scope);
+struct cache_namenode *
+  add_nblabel(void *label,
+              unsigned char labellen,
+              unsigned char node_type,
+              int isgroup,
+              uint32_t ip_addr,
+              struct nbnodename_list *scope);
+
+struct cache_namenode *
+  find_name(struct cache_namenode *namecard,
+            struct nbnodename_list *scope);
+
+#endif /* NBWORKS_NAMESRVCCACHE_H*/
+
---
+++ src/name_srvc_cache.c
@@ -0,0 +1,319 @@
+#include "c_lang_extensions.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#ifndef _POSIX_C_SOURCE
+# define _POSIX_C_SOURCE 199309
+#endif
+#include <time.h>
+
+#include <pthread.h>
+
+#include "constdef.h"
+#include "nodename.h"
+#include "pckt_routines.h"
+#include "name_srvc_pckt.h"
+#include "randomness.h"
+#include "service_sector.h"
+#include "name_srvc_cache.h"
+
+
+struct cache_scopenode nbworks_rootscope;
+
+struct {
+  unsigned int all_stop;
+} nbworks_cache_control;
+
+
+void init_name_srvc_cache() {
+  nbworks_rootscope.scope = 0;
+  nbworks_rootscope.names = 0;
+  nbworks_rootscope.next = 0;
+
+  nbworks_cache_control.all_stop = 0;
+}
+
+
+struct cache_scopenode *add_scope(struct nbnodename_list *scope,
+				  struct cache_namenode *first_node) {
+  struct cache_scopenode *result, *new_scope,
+    *cur_scope, **last_scope;
+
+  if (! scope)
+    return &nbworks_rootscope; /* NULL scope already exists. */
+
+  result = malloc(sizeof(struct cache_scopenode));
+  if (! result) {
+    /* TODO: errno signaling */
+    return 0;
+  }
+
+  result->scope = scope;
+  result->names = first_node;
+  result->next = 0;
+  new_scope = result;
+
+  while (10) {
+    last_scope = &(nbworks_rootscope.next);
+    cur_scope = nbworks_rootscope.next;
+
+    while (cur_scope) {
+      if (0 == cmp_nbnodename(scope, cur_scope->scope)) {
+	result = cur_scope;
+	if (result != new_scope)
+	  free(new_scope);
+	return result;
+      }
+
+      last_scope = &(cur_scope->next);
+      cur_scope = cur_scope->next;
+    }
+
+    *last_scope = new_scope;
+  }
+}
+
+struct cache_scopenode *find_scope(struct nbnodename_list *scope) {
+  struct cache_scopenode *result;
+
+  if (! scope)
+    return &nbworks_rootscope;
+
+  result = nbworks_rootscope.next;
+
+  while (result) {
+    if (0 == cmp_nbnodename(scope, result->scope))
+      return result;
+    else
+      result = result->next;
+  }
+
+  return result;
+}
+
+
+void *prune_scopes(void *placeholder) {
+  struct timespec waittime;
+  struct cache_scopenode *cur_scope, **last_scope, for_del2;
+  struct cache_namenode *cur_name, **last_name, *for_del;
+  struct ipv4_addr_list *cur_addr, *addr_fordel;
+  time_t curtime;
+
+  waittime.waittime.tv_sec = 1;
+  waittime.tv_nsec = 0;
+
+  while (0xbeefcafe) {
+    if (nbworks_cache_control.all_stop)
+      break;
+
+    curtime = time();
+
+    cur_scope = nbworks_rootscope.next;
+    last_scope = &(nbworks_rootscope.next);
+
+    cur_name = nbworks_rootscope.names;
+    last_name = &(nbworks_rootscope.names);
+
+    while (cur_name) {
+      if (cur_name->timeof_death < curtime) {
+	*last_name = cur_name->next;
+	for_del = cur_name;
+	cur_name = cur_name->next;
+	cur_addr = for_del->addrlist;
+	while (cur_addr) {
+	  addr_fordel = cur_addr->next;
+	  free(cur_addr);
+	  cur_addr = addr_fordel;
+	}
+	free(for_del->name);
+	free(for_del);
+      } else
+	cur_name = cur_name->next;
+    }
+
+    while (cur_scope) {
+      cur_name = cur_scope->names;
+      last_name = &(cur_scope->names);
+
+      while (cur_name) {
+	if (cur_name->timeof_death < curtime) {
+	  *last_name = cur_name->next;
+	  for_del = cur_name;
+	  cur_name = cur_name->next;
+	  cur_addr = for_del->addrlist;
+	  while (cur_addr) {
+	    addr_fordel = cur_addr->next;
+	    free(cur_addr);
+	    cur_addr = addr_fordel;
+	  }
+	  free(for_del->name);
+	  free(for_del);
+	} else
+	  cur_name = cur_name->next;
+      }
+
+      if (! cur_scope->names) {
+	*last_scope = cur_scope->next;
+	destroy_nbnodename(cur_scope->scope);
+	for_del2 = cur_scope
+	cur_scope = cur_scope->next;
+	free(for_del2);
+      } else
+	cur_scope = cur_scope->next;
+    }
+
+    nanosleep(&waittime, 0);
+  }
+
+  return 0;
+}
+
+
+/* returns: >0=succes, 0=fail (name exists), <0=error */
+struct cache_namenode *add_name(struct cache_namenode *name,
+				struct nbnodename_list *scope) {
+  struct cache_scopenode *my_scope;
+  struct cache_namenode *cur_name, **last_name;
+
+  if (! name)
+    return 0;
+
+  if (! scope)
+    my_scope = &nbworks_rootscope;
+  else
+    my_scope = find_scope(scope);
+
+  if (! my_scope)
+    /* TODO: errno signaling stuff */
+    return 0;
+
+  while (0xbeef) {
+    cur_name = my_scope->names;
+    last_name = &(my_scope->names);
+
+    while (cur_name) {
+      if ((cur_name->namelen == name->namelen) &&
+	  (0 == memcmp(cur_name->name, name->name,
+		       name->namelen)) &&
+	  (cur_name->isgroup == name->isgroup) &&
+	  (cur_name->node_type == name->node_type) &&
+	  (cur_name->dns_type == name->dns_type) &&
+	  (cur_name->dns_class == name->dns_class)) {
+	if (cur_name != name) {
+	  /* Duplicate. */
+	  return 0;
+	} else {
+	  /* Newly added one. */
+	  return name;
+	}
+      } else {
+	last_name = &(cur_name->next);
+	cur_name = cur_name->next;
+      }
+    }
+
+    *last_name = name;
+  }
+}
+
+struct cache_namenode *add_nblabel(void *label,
+				   unsigned char labellen,
+				   unsigned char node_type,
+				   int isgroup,
+				   uint32_t ip_addr,
+				   struct nbnodename_list *scope) {
+  struct cache_namenode *result;
+
+  if (! label)
+    return 0;
+
+  result = malloc(sizeof(struct cache_namenode));
+  if (! result) {
+    /* TODO: errno signaling stuff */
+    return 0;
+  }
+
+  result->name = calloc(1, labellen +1);
+  if (! result->name) {
+    /* TODO: errno signaling stuff */
+    free(result);
+    return 0;
+  }
+
+  result->addrlist = malloc(sizeof(struct ipv4_addr_list));
+  if (! result->addrlist) {
+    /* TODO: errno signaling stuff */
+    free(result->name);
+    free(result);
+    return 0;
+  }
+
+  memcpy(result->name, label, labellen);
+
+  result->addrlist->ip_addr = ip_addr;
+  result->addrlist->next = 0;
+
+  result->namelen = labellen;
+  result->node_type = node_type;
+  result->isgroup = isgroup;
+  result->dns_type = RRTYPE_NB;
+  result->dns_class = RRCLASS_IN;
+  result->timeof_death = ZEROONES; /* AKA infinity. */
+  result->endof_conflict_chance = 0;
+  result->next = 0;
+
+  /* The below code GUARANTEES insertion
+     (unless a use-after-free or similar happens). */
+
+  add_scope(scope, result);
+
+  if (add_name(result, scope)) {
+    /* Success! */
+    return result;
+  } else {
+    /* Failure. There is a duplicate. */
+    /* TODO: errno signaling stuff */
+    free(result->addrlist);
+    free(result->name);
+    free(result);
+    return 0;
+  }
+}
+
+
+struct cache_namenode *find_name(struct cache_namenode *namecard,
+				 struct nbnodename_list *scope) {
+  struct cache_scopenode *my_scope;
+  struct cache_namenode *cur_name;
+
+  if (! name)
+    return 0;
+
+  if (! scope)
+    my_scope = &nbworks_rootscope;
+  else
+    my_scope = find_scope(scope);
+
+  if (! my_scope)
+    /* TODO: errno signaling stuff */
+    return 0;
+
+  cur_name = my_scope->names;
+
+  while (cur_name) {
+    if ((cur_name->namelen == namecard->namelen) &&
+	(0 == memcmp(cur_name->name, namecard->name,
+		     namecard->namelen)) &&
+	(cur_name->isgroup == namecard->isgroup) &&
+	(cur_name->node_type == namecard->node_type) &&
+	(cur_name->dns_type == namecard->dns_type) &&
+	(cur_name->dns_class == namecard->dns_class)) {
+      return cur_name;
+    } else {
+      cur_name = cur_name->next;
+    }
+  }
+
+  return 0;
+}
