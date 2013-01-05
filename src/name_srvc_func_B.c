5846359cc68794ad35372f4eae61d99394c0c347
Aleksandar Kuktin <akuktin@gmail.com>
2013-01-05 22:11:39 +0100

    Implemented the newtid handler for NAME REGISTRATION REQUEST.

--- src/name_srvc_func_B.c
+++ src/name_srvc_func_B.c
@@ -23,6 +23,7 @@
 #include "name_srvc_func_B.h"
 #include "randomness.h"
 #include "service_sector.h"
+#include "service_sector_threads.h"
 
 
 /* return: 0=success, >0=fail, -1=error */
@@ -293,14 +294,143 @@ struct name_srvc_resource *name_srvc_B_callout_name(unsigned char *name,
 
 void *name_srvc_B_handle_newtid(void *input) {
   struct timespec sleeptime;
-  struct sockaddr_in addr;
   struct newtid_params params;
-  struct name_srvc_packet *pckt, *outside_pckt;
+  struct thread_node *last_will;
+
+  struct name_srvc_packet *pckt;
+  struct ss_name_pckt_list *outside_pckt, *last_outpckt;
+
+  struct name_srvc_resource_lst *res;
+  struct cache_namenode *cache_namecard;
+  struct nbaddress_list *nbaddr_list;
+  uint32_t in_addr;
+  unsigned char octet, label[NETBIOS_NAME_LEN+1], label_type;
+
   void *result;
+
 
   memcpy(&params, input, sizeof(struct newtid_params));
 
-  //  ...
+  if (params.thread_id)
+    last_will = add_thread(params.thread_id);
+  else
+    last_will = 0;
+
+  /* TODO: change this to a global setting. */
+  sleeptime.tv_sec = 0;
+  sleeptime.tv_nsec = T_250MS;
+
+  label[NETBIOS_NAME_LEN] = '\0';
+
+  ss_set_inputdrop_name_tid(params.tid);
+
+  last_outpckt = 0;
+  do {
+    outside_pckt = ss_name_recv_entry(params.trans);
+
+    if (outside_pckt == last_outpckt) {
+      /* Error. If the function was called, then
+	 there was supposed to be at least one
+	 packet waiting for it. */
+      free(outside_pckt);
+      ss_deregister_name_tid(params.tid);
+      ss_name_dstry_recv_queue(params.trans);
+      if (last_will)
+	last_will->dead = 9001; /* It's OVER *9000*!!! */
+      return 0;
+    } else {
+      if (last_outpckt)
+	free(last_outpckt);
+      last_outpckt = outside_pckt;
+    }
+
+  } while (! outside_pckt->packet);
+
+  if ((outside_pckt->packet->header->opcode == (OPCODE_REQUEST |
+						OPCODE_REGISTRATION)) &&
+      (! outside_pckt->packet->header->rcode)) {
+    /* NAME REGISTRATION REQUEST */
+
+    for (res = outside_pckt->packet->aditionals;
+	 res != 0;
+	 res = res->next) {
+      if (res->res) {
+
+	if ((res->res->name) &&
+	    (res->res->rdata_t == nb_address_list)) {
+
+	  nbaddr_list = res->res->rdata;
+	  while (nbaddr_list) {
+	    if (nbaddr_list->flags & NBADDRLST_GROUP_MASK)
+	      octet = 1;
+	    else
+	      octet = 0;
+
+	    cache_namecard = find_nblabel(res->res->name->name,
+					  res->res->name->len,
+					  ANY_NODETYPE, octet,
+					  res->res->rrtype,
+					  res->res->rrclass,
+					  res->res->name->next_name);
+
+	    if (cache_namecard)
+	      if (cache_namecard->ismine &&
+		  (0 == cache_namecard->isgroup)) {
+		/* Someone is trying to take my name. */
+
+		memcpy(&label, cache_namecard->label, NETBIOS_NAME_LEN);
+		label_type = label[NETBIOS_NAME_LEN-1];
+		label[NETBIOS_NAME_LEN-1] = '\0';
+		if (cache_namecard->addrlist)
+		  in_addr = cache_namecard->addrlist->ip_addr;
+		else
+		  in_addr = 0;
+
+		pckt = name_srvc_make_name_reg_small(label, label_type,
+						     res->res->name->next_name,
+						     cache_namecard->ttl,
+						     in_addr, 0,
+						     cache_namecard->node_type);
+		pckt->header->opcode = (OPCODE_RESPONSE & OPCODE_REGISTRATION);
+		pckt->header->nmflags = FLG_AA;
+		pckt->header->rcode = RCODE_REGISTR_ACT_ERR;
+		ss_name_send_pckt(pckt, outside_pckt->addr, params.trans);
+
+		destroy_name_srvc_pckt(pckt, 0, 1);
+		destroy_name_srvc_pckt(outside_pckt->packet, 1, 1);
+		free(outside_pckt);
+		ss_deregister_name_tid(params.tid);
+		ss_name_dstry_recv_queue(params.trans);
+		if (last_will)
+		  last_will->dead = 9000; /* Not quite as furious, but still. */
+
+		/* Now, the subject of a return from here is interesting.
+		   What if there are other packets with the same tid?
+		   What if there is something _important_ in those packets?
+		   Well, I chose to ignore them, so tough luck. */
+		/* Recomended excuse or explanation: "UDP is unreliable." */
+		return 0;
+
+	      }
+
+	    nbaddr_list = nbaddr_list->next_address;
+	  }
+	}
+      }
+    }
+  }
+
+  // NAME QUERY REQUEST
+
+  // POSITIVE NAME QUERY RESPONSE
+
+  // NAME CONFLICT DEMAND
+
+  // NAME RELEASE REQUEST
+
+  // NAME UPDATE REQUEST
+
+  // NODE STATUS REQUEST
 
   return 0;
 }
