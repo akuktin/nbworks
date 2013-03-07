13771047a94ce1ac4af91876aef114030529ced8
Aleksandar Kuktin <akuktin@gmail.com>
2013-03-07 18:36:52 +0100

    I'm an idiot.

--- src/name_srvc_pckt.c
+++ src/name_srvc_pckt.c
@@ -589,8 +589,7 @@ unsigned char *fill_name_srvc_resource_data(struct name_srvc_resource *content,
   unsigned char *walker;
 
   if ((! (content && field)) ||
-      (! end_of_packet) ||
-      (! field))
+      (field > end_of_packet))
     return field;
 
   walker = field;
