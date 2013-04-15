a2a9cace7ab22b1564f23188cd1bcb6cb246d322
Aleksandar Kuktin <akuktin@gmail.com>
2013-04-15 23:39:45 +0200

    Implemented init_my_ip4_address() for Linux.
    It may have quite a few problem in hosts with more than one net interface.

--- src/portability.c
+++ src/portability.c
@@ -20,6 +20,11 @@
 
 #include <stdint.h>
 
+#ifdef SYSTEM_IS_LINUX
+# include <sys/ioctl.h>
+# include <net/if.h>
+#endif
+
 #ifdef COMPILING_NBNS
 # ifndef COMPILING_DAEMON
 #  define COMPILING_DAEMON 1
@@ -33,9 +38,13 @@
 # include "nbworks.h"
 # include "constdef.h"
 #endif
+#include "pckt_routines.h"
 
+
+#define NUMOF_REQUESTS 32
 
 #ifdef COMPILING_DAEMON
+# ifdef SYSTEM_IS_LINUX
 ipv4_addr_t init_default_nbns(void) {
   /* FORRELEASE: This has to be changed, somehow. */
   /* No srsly, how do I do this? If the config file is empty? */
@@ -45,7 +54,9 @@ ipv4_addr_t init_default_nbns(void) {
 
   return nbworks__default_nbns;
 }
+# endif
 
+# ifdef SYSTEM_IS_LINUX
 ipv4_addr_t init_brdcts_addr(void) {
   // FORRELEASE: stub
   //        192.168.1.255/24
@@ -54,19 +65,63 @@ ipv4_addr_t init_brdcts_addr(void) {
 
   return brdcst_addr;
 }
-#endif
+# endif
+#endif /* COMPILING_DAEMON */
 
+#ifdef SYSTEM_IS_LINUX
 ipv4_addr_t init_my_ip4_address(void) {
-  // FORRELEASE: stub
-  //        192.168.1.2/24
+  struct ifreq request[NUMOF_REQUESTS];
+  struct ifconf for_ioctl;
+  struct sockaddr_in *addr_p;
+  int count, sckt;
 
-  nbworks__myip4addr = 0xc0a80102;
+  for_ioctl.ifc_len = (sizeof(struct ifreq) * NUMOF_REQUESTS);
+  for_ioctl.ifc_req = request;
+
+  memset(&request, 0, (sizeof(struct ifreq) * NUMOF_REQUESTS));
+
+  sckt = socket(PF_INET, SOCK_DGRAM, 0);
+  if (sckt == -1) {
+    nbworks__myip4addr = 0;
+    nbworks_errno = ADD_MEANINGFULL_ERRNO;
+    return 0;
+  }
+
+  if (0 > ioctl(sckt, SIOCGIFCONF, &for_ioctl)) {
+    close(sckt);
+
+    nbworks__myip4addr = 0;
+    nbworks_errno = ADD_MEANINGFULL_ERRNO;
+    return 0;
+  }
+
+  close(sckt);
+
+  for (count = 0; count < (for_ioctl.ifc_len / sizeof(struct ifreq));
+       count ++) {
+    /* Weed out the loopback interface. */
+    if (0 != strcmp(request[count].ifr_name, "lo\0")) {
+      break;
+    }
+  }
+
+  if (count < NUMOF_REQUESTS) {
+    addr_p = (struct sockaddr_in *)&(request[count].ifr_addr);
+    read_32field((unsigned char *)&(addr_p->sin_addr.s_addr),
+		 &nbworks__myip4addr);
+  } else {
+    nbworks__myip4addr = 0;
+  }
+    
   return nbworks__myip4addr;
 }
+#endif
 
+#ifdef SYSTEM_IS_LINUX
 /* return: 0 = success; !0 = !success */
 int set_sockoption(int socket,
 		   unsigned int what) {
   // FORRELEASE: nonexistant
   return 0;
 }
+#endif
